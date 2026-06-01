7b1a2069bbf5247f6830adf8d486eeb656e2d454
myumelldy1 <myumelldy1@gmail.com>
2026-06-02 03:54:11 +0700

    Update saweria.js

--- api/saweria.js
+++ api/saweria.js
@@ -1,5 +1,11 @@
 export default async function handler(req, res) {
 
+  if (req.method === "GET") {
+    return res.status(200).json({
+      status: "Webhook aktif"
+    });
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
